c399c785e252aa6a059c05f76866b8eab52b6c96
hqwrong <hq.wrong@gmail.com>
2014-12-23 23:41:16 +0800

    recover icon

--- js/libs/weixin_share.js
+++ js/libs/weixin_share.js
@@ -12,21 +12,19 @@ $(function(){
             return false;
         }
     })();
-    if(!isWeixinBrowser) {
-        $('.'+weixinShareClass).hide();
-        return;
-    }
     $('.'+weixinShareClass).click(function(){
-        var layer = $('#__for_share');
-        if(!layer.length){
-            var div = document.createElement('div');
-            div.id = '__for_share';
-            $(div).html('<div class="share_img"></div>').hide().appendTo(document.body);
-            layer = $('#__for_share');
-            layer.click(function(){
-                layer.hide();
-            });
+        if (isWeixinBrowser) {
+            var layer = $('#__for_share');
+            if(!layer.length){
+                var div = document.createElement('div');
+                div.id = '__for_share';
+                $(div).html('<div class="share_img"></div>').hide().appendTo(document.body);
+                layer = $('#__for_share');
+                layer.click(function(){
+                    layer.hide();
+                });
+            }
+            layer.show();
         }
-        layer.show();
     });
 });
