a76240f91984f8c486a4e1f40763d201d017319f
hqwrong <hq.wrong@gmail.com>
2018-01-26 18:00:03 +0800

    update

--- js/cover.js
+++ js/cover.js
@@ -1,7 +1,7 @@
 // this file is auto generated.
 // to random a cover image.
 
-var covers = [['/images/covers/2511753488.jpg', '2017.10  日本 京都 金地院'], ['/images/covers/2431442130.jpg', ''], ['/images/covers/2431442301.jpg', ''], ['/images/covers/2511753621.jpg', '2017  惠州 大南山'], ['/images/covers/2431442318.jpg', ''], ['/images/covers/2431584622.jpg', ''], ['/images/covers/2431441939.jpg', ''], ['/images/covers/2431442338.jpg', '海洋世界.珠海'], ['/images/covers/2431584680.jpg', '2016 高雄.台湾'], ['/images/covers/2431442034.jpg', ''], ['/images/covers/2431441843.jpg', '2016 花莲 台湾  七星潭'], ['/images/covers/2431442046.jpg', ''], ['/images/covers/2511754082.jpg', '京都 金地院'], ['/images/covers/2431441940.jpg', '2016 长湴公园.广州'], ['/images/covers/2431584704.jpg', '']]
+var covers = [['/images/covers/2431442046.jpg', ''], ['/images/covers/2431442130.jpg', ''], ['/images/covers/2431441940.jpg', '长湴公园'], ['/images/covers/2431441939.jpg', ''], ['/images/covers/2431442338.jpg', '珠海'], ['/images/covers/2511755732.jpg', ''], ['/images/covers/2431584680.jpg', '高雄'], ['/images/covers/2431442318.jpg', ''], ['/images/covers/2431584622.jpg', ''], ['/images/covers/2511753621.jpg', '大南山'], ['/images/covers/2431584704.jpg', ''], ['/images/covers/2431442301.jpg', ''], ['/images/covers/2511754082.jpg', '金地院'], ['/images/covers/2431441843.jpg', '七星潭'], ['/images/covers/2431442034.jpg', '']]
 
     var i = Math.floor(Math.random() * covers.length);
     
